539ce78f6faca547cc1584f7312a4956aac164fb
Igor Akulov <egor@akulovs.com>
2015-04-10 10:09:48 +0500

    first refactor

--- app/hytranscoder.js
+++ app/hytranscoder.js
@@ -3,37 +3,24 @@ var express = require('express'),
 
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+/*app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(req, res) {
   res.send('index.html');
-});
-
-app.get('/flash/:filename', function(req, res) {
-  res.contentType('flv');
-  // make sure you set the correct path to your video file storage
-  var pathToMovie = __dirname + '/media/' + req.params.filename;
-  var proc = ffmpeg(pathToMovie)
-    // use the 'flashvideo' preset (located in /lib/presets/flashvideo.js)
-    .preset('flashvideo')
-    // setup event handlers
-    .on('end', function() {
-      console.log('file has been converted succesfully');
-    })
-    .on('error', function(err) {
-      console.log('an error happened: ' + err.message);
-    })
-    // save to stream
-    .pipe(res, {end:true});
-});
+});*/
 
 app.get('/webm/:filename', function(req, res) {
   res.contentType('video/webm');
   // make sure you set the correct path to your video file storage
-  var pathToMovie = __dirname + '/media/' + req.params.filename;
+  var pathToMovie = __dirname +  '/media/' + req.params.filename;
+  var offset = req.query.offset;     if (offset.length === 0) {offset = 0}
+  var rtmpStream = req.query.stream; if (rtmpStream.length === 0) {rtmpStream='teacher'}
+  var rtmpServer = req.query.server; if (rtmpServer.length === 0) {rtmpServer = 'hybinar.ru/live'}
+  console.log('offset: ', offset);
+  console.log('rtmpServer: ', 'rtmp://' + rtmpServer + '/' + rtmpStream);
   var proc = ffmpeg(pathToMovie)
     /* Output to Hybinar server */
-      .output('rtmp://hybinar.ru/live/egor')
+      .output('rtmp://' + rtmpServer + '/' + rtmpStream)
       .format('flv')
       .audioCodec('libvo_aacenc')
       .audioBitrate('64k')
@@ -57,9 +44,8 @@ app.get('/webm/:filename', function(req, res) {
       .addOption('-cpu-used', '4')
       .addOption('-deadline', 'realtime')
       .addOption('-bufsize', '512')
-      .on('end', function() {console.log('file has been converted succesfully');})
-      .on('error', function(err) {console.log('an error happened: ' + err.message);})
+      .on('end', function() {console.log('End of file reached');})
+      .on('error', function(err) {"ffmpeg instance destroyed, ", console.log(err.message);})
       .run();
 });
-
-app.listen(4000);
+app.listen(5000);
